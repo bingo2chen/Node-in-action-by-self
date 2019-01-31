be2a83afb1ab833c2a1bde759061cefcbe1b4e7a
ghosThot <1018513521@qq.com>
2019-01-31 18:44:12 +0800

    use nunjucks

--- later/index.js
+++ later/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const read = require('node-readability');
+const nunjucks = require('nunjucks');
 const Article = require('./db').Article;
 
+const app = express();
+
+const url = 'http://www.manning.com/cantelon2/';
+
 app.set('port', process.env.PORT || 3000);
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/articles', (req, res, next) => {
   Article.all((err, articles) => {
     if (err) return next(err);
-    res.send(articles);
+    res.render('articles.nj', {articles: articles});
   });
 });
 
@@ -19,14 +29,22 @@ app.get('/articles/:id', (req, res, next) => {
   const id = req.params.id;
   Article.find(id, (err, article) => {
     if (err) return next(err);
-    res.send(article[id]);
+    res.send(article);
   });
 });
 
 app.post('/articles', (req, res, next) => {
-  const article = { title: req.body.title };
-  articles.push(article);
-  res.send(articles);
+  const url = req.body.url;
+  read(url, (err, result) => {
+    if (err || !result) res.status(500).send('Error downloading article');
+    Article.create(
+      { title: result.title, content: result.content },
+      (err, article) => {
+        if (err) return next(err);
+        res.send('OK');
+      }
+    )
+  })
 });
 
 app.delete('/articles/:id', (req, res, next) => {
